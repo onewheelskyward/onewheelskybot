ff747ad5fe68de9f8053a022c0f1cd401ac8cedb
Andrew Kreps <andrew.kreps@gmail.com>
2013-09-17 12:19:19 -0700

    Added config.yml.dist & yaml parser.

--- bot.rb
+++ bot.rb
@@ -2,34 +2,37 @@ require 'cinch'
 require 'mechanize'
 require 'open-uri'
 require 'json'
+require 'yaml'
+
+config = YAML.load_file('config.yml')
 
 def handle_image(search_term)
-	url = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=#{URI::encode search_term}"
-	agent = Mechanize.new
-	agent.get(url) do |result|
-		parsed = JSON.parse result.body
-		return parsed['responseData']['results'][0]['url']
-	end
+  url = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=#{URI::encode search_term}"
+  agent = Mechanize.new
+  agent.get(url) do |result|
+    parsed = JSON.parse result.body
+    return parsed['responseData']['results'][0]['url']
+  end
 end
 
 bot = Cinch::Bot.new do
   configure do |c|
-    c.server = "pucksteak"
-	c.nick = "onewheelskybot"
-	c.user = "onewheelskybot"
-	c.realname = "onewheelskybot"
-	c.password = "password"
-	c.port = 1919
+    c.server = config['server']
+    c.nick = config['nick']
+    c.user = config['user']
+    c.realname = config['realname']
+    c.password = config['password']
+    c.port = config['port']
 
     #c.channels = ["#pdxbots"]
   end
 
-  on :message, /onewheelskybot (.*)/ do |m, element|
+  on :message, /#{config['nick']} (.*)/ do |m, element|
     case element
-		when /^image/i
-			reply = handle_image(element.gsub /image\s*m*e*\s*/, '')
-	end
-	m.reply reply
+      when /^image/i
+        reply = handle_image(element.gsub /image\s*m*e*\s*/, '')
+    end
+    m.reply reply  if reply
   end
 end
 
--- run.rb
+++ run.rb
@@ -1,8 +1,8 @@
-Dir.glob("models/*.rb").each { |model| require_relative model }
+require 'data_mapper'
+#Dir.glob("models/*.rb").each { |model| require_relative model }
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, "sqlite::memory:")
 DataMapper.finalize
 DataMapper.auto_migrate!
 
 require_relative 'bot'
-

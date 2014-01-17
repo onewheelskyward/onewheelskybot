7fa87a61923d27d6a78b6d0b1ac7e42816ac3087
Andrew Kreps <andrew.kreps@gmail.com>
2014-01-17 14:39:10 -0800

    Start of location-based weather

--- helpers.rb
+++ helpers.rb
@@ -1,5 +1,6 @@
 
 def get_personalized_query(user, key, query)
+  #location = get_location_from_zrobo(user)
   if query != ''
     UserStore.create(user: user, keything: key, value: query)
   else
@@ -9,4 +10,8 @@ def get_personalized_query(user, key, query)
     end
   end
   query
+end
+
+def get_location_from_zrobo(user)
+  # http://icecondor.com/donpdonp.json
 end

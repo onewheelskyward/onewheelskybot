1c8bd3ed8873e4f1caa7b54ef340b10b1726b1b4
Andrew Kreps <andrew.kreps@gmail.com>
2014-01-21 18:46:37 -0800

    Fixed a bug with the temp text code.

--- plugins/forecast_io.rb
+++ plugins/forecast_io.rb
@@ -44,14 +44,11 @@ class ForecastIO
     request = params[:Body]
     case request
       when /^forecast/i
-        text = bot.plugins[4].get_weather_forecast(request.gsub /^forecast\s*/i, '')
-      #when /^rain/i
-      #  text = bot.plugins[4].do_the_ascii_thing(request.gsub /^asciirain\s*/i, '')
+        text = bot.plugins[4].get_weather_forecast(request.sub /^forecast\s*/i, '')
       when /^rain/i
-        text = bot.plugins[4].do_the_precip_thing(request.gsub /^rain\s*/i, '')
+        text = bot.plugins[4].do_the_precip_thing request.sub(/^rain\s*/i, '')
       when /^temp/i
-        puts request.inspect
-        text = bot.plugins[4].do_the_temp_thing(request.to_s.gsub /^temp\s*/i, @@ansi_chars)
+        text = bot.plugins[4].do_the_temp_thing(request.sub(/^temp\s*/i, ''), @@ansi_chars)
       when /^say/i
         text = request.sub /^say /i, ''
         bot.reply

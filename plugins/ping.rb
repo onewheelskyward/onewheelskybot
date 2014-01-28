f24ac10b9b54e15fc3e1539ebec308afd6826ce1
Andrew Kreps <andrew.kreps@gmail.com>
2014-01-28 14:08:53 -0800

    PONG

--- plugins/ping.rb
+++ plugins/ping.rb
@@ -10,23 +10,28 @@ class Ping
   EOF
 
   def pong(msg)
-    pong_images = %w(http://img209.imageshack.us/img209/366/toast.gif
-                     http://i.imgur.com/cRgZZca.gif
-                     https://dl.dropboxusercontent.com/u/575564/apecgnU.gif
-                     http://i.imgur.com/1LG3p1Q.gif
-                     http://cdn.memegenerator.net/instances/500x/43965451.jpg
-                     http://30.media.tumblr.com/tumblr_m3056cCdb71rr3l61o1_500.gif
-                     http://giphy.com/gifs/Us2YBZNhO8Pba/tiled
-                     http://media.giphy.com/media/2WhCMpG85VhWE/giphy.gif
-                     http://media.giphy.com/media/rUCzODGkQF8GY/giphy.gif
-                     http://i.imgur.com/SbW8sEh.gif
-                     http://i.imgur.com/2mBqBxw.jpg
-                     http://i.imgur.com/YTC0CEK.gif
-                     http://swng.it/uYr6s.gif
-                     http://www.youtube.com/watch?v=Xb5iqUhC-kA
-                     http://25.media.tumblr.com/fa6c3e51a6464e202c351c607a4ea7b9/tumblr_mqpg9st8K71snfsquo1_400.gif
-                     http://i.imgur.com/b0GVMNz.gif
-                     http://www.doggifpage.com/gifs/109.gif
+    pong_images = %w( http://img209.imageshack.us/img209/366/toast.gif
+                      http://i.imgur.com/cRgZZca.gif
+                      https://dl.dropboxusercontent.com/u/575564/apecgnU.gif
+                      http://i.imgur.com/1LG3p1Q.gif
+                      http://cdn.memegenerator.net/instances/500x/43965451.jpg
+                      http://30.media.tumblr.com/tumblr_m3056cCdb71rr3l61o1_500.gif
+                      http://giphy.com/gifs/Us2YBZNhO8Pba/tiled
+                      http://media.giphy.com/media/2WhCMpG85VhWE/giphy.gif
+                      http://media.giphy.com/media/rUCzODGkQF8GY/giphy.gif
+                      http://i.imgur.com/SbW8sEh.gif
+                      http://i.imgur.com/2mBqBxw.jpg
+                      http://i.imgur.com/YTC0CEK.gif
+                      http://swng.it/uYr6s.gif
+                      http://www.youtube.com/watch?v=Xb5iqUhC-kA
+                      http://25.media.tumblr.com/fa6c3e51a6464e202c351c607a4ea7b9/tumblr_mqpg9st8K71snfsquo1_400.gif
+                      http://i.imgur.com/b0GVMNz.gif
+                      http://www.doggifpage.com/gifs/109.gif
+                      http://i.imgur.com/aVn5ABf.gif
+                      http://swng.it/14ZqYT.gif
+                      http://i.imgur.com/2PjC7RX.gif
+http://i.imgur.com/82FbFvi.gif
+https://dl.dropboxusercontent.com/u/575564/tumblr_mppsh4IaB81snfsquo1_400.gif
                     )
     msg.reply("#{pong_images.sample}")
   end

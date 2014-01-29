647d22d5c12f8388da4bcbdc5431e057b6c73712
Andrew Kreps <andrew.kreps@gmail.com>
2014-01-29 09:28:18 -0800

    PONG

--- plugins/ping.rb
+++ plugins/ping.rb
@@ -32,6 +32,7 @@ class Ping
                       http://i.imgur.com/82FbFvi.gif
                       https://dl.dropboxusercontent.com/u/575564/tumblr_mppsh4IaB81snfsquo1_400.gif
                       http://0.media.collegehumor.cvcdn.com/80/11/a5514aa0d1ad89720722b4fc12a9fe12.gif
+                      http://media1.break.com/dnet/media/2013/3/29/3d7c8a6a-e7df-41f0-9526-038224f4baa5.gif
                     )
     msg.reply("#{pong_images.sample}")
   end

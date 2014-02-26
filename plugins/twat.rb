f8bcd98e7130b6da2267c4c352bb663127f8a304
Andrew Kreps <andrew.kreps@gmail.com>
2014-02-25 21:15:51 -0800

    Baseline twitter and someone else's twitter.

--- plugins/twat.rb
+++ plugins/twat.rb
@@ -5,6 +5,7 @@ class Twitter
   include Cinch::Plugin
 
   match /(https:\/\/twitter.com\/\w+\/status\/\d+)/, use_prefix: false, method: :execute
+  match /(https:\/\/twitter.com\/\w+)/, use_prefix: false, method: :execute_profile
 
   set :help, <<-EOF
 !twitter [url]
@@ -17,5 +18,14 @@ class Twitter
     username = username_node[1].content # First <b> is blank.  /me shrugs
     tweet_text_nodes = noko.css('p.tweet-text') # .each here gives the whole conversation.
     msg.reply "@#{username} #{tweet_text_nodes[0].content}"
+  end
+
+  def execute_profile(msg, query)
+    resp = HTTParty.get(query)
+    noko = Nokogiri::HTML resp.body
+    username_node = noko.css('span.username b')
+    username = username_node[1].content # First <b> is blank.  /me shrugs
+    profile_node = noko.css('p.profile-field')
+    msg.reply "@#{username} #{profile_node[0].content}"
   end
 end

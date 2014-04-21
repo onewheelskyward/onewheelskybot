81483010e7f7d32ce4e2ed77bddbdffac1069234
Andrew Kreps <andrew.kreps@gmail.com>
2014-04-20 23:05:33 -0700

    ForecastIO test updates.

--- tests/forecast_io_tests.rb
+++ tests/forecast_io_tests.rb
@@ -5,9 +5,9 @@ describe 'forecastio controller tests' do
   include Cinch::Test
   let(:bot) { make_bot(ForecastIO, :foo => 'foo_value') }
 
-  it 'tests the texts' do
-    HTTParty.get('http://localhost:6207/forecast')
-    "x".should == "y"
-  end
+  # it 'tests the texts' do
+  #   HTTParty.get('http://localhost:6207/forecast')
+  #   "x".should == "y"
+  # end
 
 end

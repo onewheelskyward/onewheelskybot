7dc4ce7cb13e9c077d529662b61b9a37f24643c8
Andrew Kreps <andrew.kreps@gmail.com>
2014-07-13 00:04:58 -0700

    Fixing texting signatures.

--- plugins/forecast_io.rb
+++ plugins/forecast_io.rb
@@ -58,7 +58,7 @@ Once you specify a location, it will persist as long as you own your nick.
     request, query = params[:Body].split ' '
 
     query = get_personalized_query('twillio text', 'weather', query)
-    forecast = ForecastIOMethods::get_forecast_io_results query
+    forecast = bot.plugins[4].get_forecast_io_results query
 
     case request
       when /^forecast/i
@@ -66,9 +66,9 @@ Once you specify a location, it will persist as long as you own your nick.
       when /^rain/i
         text = bot.plugins[4].sms_rain_forecast request.sub(/^rain\s*/i, '')
       when /^temp/i
-        text = bot.plugins[4].do_the_temp_thing(forecast, request, %w[_ ▁ ▃ ▅ ▇ █])
+        text = bot.plugins[4].do_the_temp_thing(forecast, %w[_ ▁ ▃ ▅ ▇ █], 24)
       when /^cond/i
-        text = bot.plugins[4].conditions(forecast, request, %w[_ ▁ ▃ ▅ ▇ █])
+        text = bot.plugins[4].conditions(forecast)
       when /^say/i
         text = request.sub /^say /i, ''
         bot.Channel('#pdxtech').send(text)

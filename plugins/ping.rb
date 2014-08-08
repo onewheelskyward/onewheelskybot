efaf304cae6c559de05badb6a40783f149fe38a2
Andrew Kreps <andrew.kreps@gmail.com>
2014-08-08 11:13:52 -0700

    PONG

--- plugins/ping.rb
+++ plugins/ping.rb
@@ -113,6 +113,7 @@ class Ping
                       http://giant.gfycat.com/InbornGrandioseAfricanporcupine.gif
                       http://37.media.tumblr.com/b677b6b70be6235fbfe8797d6ce59734/tumblr_n6euc1ZIIw1txy0q2o1_400.gif
                       http://i.imgur.com/s6bjPGC.gif
+                      http://i.hizliresim.com/wvPaVv.jpg
                   )
     if msg.user.nick == 'incanus77'
       pong = %w(

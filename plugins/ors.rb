df3942d6dc5b7342df951da366f9985b4cc78b55
Andrew Kreps <andrew.kreps@gmail.com>
2014-10-28 00:36:33 -0700

    Oregon Revised Statues

---
+++ plugins/ors.rb
@@ -0,0 +1,21 @@
+###
+# Work in progress.  There's so much data, it's hard to display reasonably in IRC.
+##
+# require 'json'
+# require 'httparty'
+#require
+
+class ORS
+  include Cinch::Plugin
+
+  match /!ors\s*(\d+\.\d+)$/i, method: :execute
+
+  set :help, <<-EOF
+This will simply redirect you to oregonlaws.org.
+!ors xxx.xxx
+  EOF
+
+  def execute(msg, query)
+      msg.reply "http://www.oregonlaws.org/ors/#{query}"
+  end
+end
